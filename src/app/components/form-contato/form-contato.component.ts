bd101a59ebc0dc9c24553ae0313fd19d23b44f21
yurikb <yuri.kollerborba@gmail.com>
2022-03-04 23:45:13 -0300

    updates

--- src/app/components/form-contato/form-contato.component.ts
+++ src/app/components/form-contato/form-contato.component.ts
@@ -1,32 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 import { ViaCepModel } from 'src/app/models/via-cep-model';
 import { ViaCepApiService } from 'src/app/services/via-cep-api.service';
 
 @Component({
   selector: 'app-form-contato',
   templateUrl: './form-contato.component.html',
-  styleUrls: ['./form-contato.component.scss']
+  styleUrls: ['./form-contato.component.scss'],
 })
 export class FormContatoComponent implements OnInit {
-
   formContato: ViaCepModel = {};
 
-  showForm: boolean = false;
+  showForm = new Subject<boolean>();
 
-  constructor(private cepService: ViaCepApiService) { }
+  constructor(private cepService: ViaCepApiService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getViaCEP(cep: FocusEvent) {
+    if ((cep.target as HTMLInputElement)?.value) {
+      let inputCEP = (cep.target as HTMLInputElement)?.value;
+      console.log(inputCEP);
+      const cepResponse = this.cepService.getCep(inputCEP);
+      cepResponse.subscribe((cepModel) => {
+        this.formContato = cepModel;
+        this.showForm.next(true);
+      });
+    }
   }
-
-  getViaCEP(cep:any) {
-    console.log(cep);
-    this.cepService
-      .getCep(cep)
-      .subscribe(
-        (response) => {
-          console.log(response);
-        }
-      );
-  }
-
 }
--- src/app/models/via-cep-model.ts
+++ src/app/models/via-cep-model.ts
@@ -1,17 +1,17 @@
 export class ViaCepModel {
-    cep?: string;
-    logradouro?: string;
-    complemento?: string;
-    bairro?: string;
-    localidade?: string;
-    uf?: string;
-    ibge?: string;
-    gia?: string;
-    ddd?: string;
-    siafi?: string;
+  cep?: string;
+  logradouro?: string;
+  complemento?: string;
+  bairro?: string;
+  localidade?: string;
+  uf?: string;
+  ibge?: string;
+  gia?: string;
+  ddd?: string;
+  siafi?: string;
+  numero?: number;
 
-    constructor(object: Partial<ViaCepModel>) {
-        Object.assign(this, object);
-    }
-
-} 
+  constructor(object: Partial<ViaCepModel>) {
+    Object.assign(this, object);
+  }
+}
